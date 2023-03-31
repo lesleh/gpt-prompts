bcff3e42f306c7fc9c48af7da664c47b9dc6c1f1
Leslie Hoare <iam@lesleh.co.uk>
2023-03-31 16:09:09 +0100

    Implement API Extractor and generate types
    Add API Extractor config and run it to generate type declarations
    and API reports. Temporarily exclude API reports by adding them to
    .gitignore. Update TS configs to correspond to new folder
    structure. Add a sample exported function with TSDoc comments.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { OpenAIApi } from "openai";
 
 console.log(OpenAIApi);
 
+/**
+ * Adds two numbers
+ * @public
+ * @param a - first number
+ * @param b - second number
+ * @returns sum of a and b
+ */
 export function add(a: number, b: number): number {
   return a + b;
 }
