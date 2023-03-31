839b059e0fdc11d1091b520c519b46f0d09b3ce6
Leslie Hoare <iam@lesleh.co.uk>
2023-03-31 18:02:45 +0100

    Add parser for Prompt and PromptVariables
    This commit adds functionality to parse Prompt and PromptVariables
    from a given set of input. It includes the following changes:
    
    - Add 'src/parser.spec.ts' with tests for parsePrompt and
      parsePromptVariables functions
    - Add 'src/parser.ts' with the main parsing functions for creating
      prompts and handling variables
    - Update 'src/types.ts' to include ChatCompletionRequestMessage
      in message types
    - Add optional default values for PromptVariable objects

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,23 @@
-import { ChatCompletionResponseMessage } from "openai";
+import {
+  ChatCompletionRequestMessage,
+  ChatCompletionResponseMessage,
+} from "openai";
 
 export type PromptVariable = {
   name: string;
   type: string;
   description: string;
-  default: string;
+  default?: string;
   required: boolean;
 };
 
 export type Prompt = {
   name: string;
   description: string;
-  message: string;
+  message: string | ChatCompletionRequestMessage[];
   variables: Record<string, PromptVariable>;
 };
 
 export type PromptResult = {
-  name: string;
   messages: ChatCompletionResponseMessage[];
 };
