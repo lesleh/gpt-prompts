b21c39d6d155959e58ee7a4124da8edd9457f9f0
Leslie Hoare <iam@lesleh.co.uk>
2023-04-01 10:32:04 +0100

    Add ESLint configuration and ignore file
    This commit adds a new `.eslintignore` file with folders to be
    ignored by ESLint, and a `.eslintrc.js` file with the ESLint
    configuration. Additionally, a "lint" script is added to the
    `package.json` file for easy usage.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  overrides: [
+    {
+      files: ".eslintrc.js",
+      env: {
+        node: true,
+      },
+    },
+  ],
+};
