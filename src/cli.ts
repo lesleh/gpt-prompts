27be302dcb4524dbfdaadd075aff7717e77ae09c
Leslie Hoare <iam@lesleh.co.uk>
2023-04-01 11:54:11 +0100

    Add CLI functionality and update package.json
    Added a new file src/cli.ts to handle CLI functionality along with
    updating the package.json to include a new "bin" entry. The "bin"
    entry points to the dist/cjs/cli.js file for executing the
    gpt-prompts CLI tool. The src/cli.ts file contains functions to convert
    prompts to inquirer questions, select a prompt, and parse the prompt
    based on user input.

---
+++ src/cli.ts
@@ -0,0 +1,75 @@
+import { characterImpersonator } from "./prompts";
+import { Prompt, PromptVariable } from "./types";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - This errors for the CJS build but works for the ESM build
+import { type Question } from "inquirer";
+import { parsePrompt } from ".";
+
+function convertPromptToInquirerQuestions(prompt: Prompt): Question[] {
+  return Object.values(prompt.variables).map((variable: PromptVariable) => ({
+    // TODO: Handle other types
+    type: variable.type === "string" ? "input" : "input",
+    name: variable.name,
+    message: variable.description,
+    default: variable.default,
+  }));
+}
+
+async function selectPrompt(prompts: Prompt[]) {
+  // TODO: Move this to the static import once CJS build is removed
+  // Ref: https://github.com/lesleh/gpt-prompts/issues/12
+  const inquirer = (await import("inquirer")).default;
+
+  // Select the prompt
+  const promptChoices = prompts.map((prompt) => ({
+    name: `${prompt.name} - ${prompt.description}`,
+    value: prompt,
+  }));
+
+  const { selectedPrompt } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "selectedPrompt",
+      message: "Select a prompt:",
+      choices: promptChoices,
+    },
+  ]);
+
+  return selectedPrompt;
+}
+
+async function main() {
+  // TODO: Move this to the static import once CJS build is removed
+  // Ref: https://github.com/lesleh/gpt-prompts/issues/12
+  const inquirer = (await import("inquirer")).default;
+
+  const selectedPrompt = await selectPrompt([
+    // ... other prompts
+    characterImpersonator,
+  ]);
+
+  // Ask the user for input based on the selected prompt
+  const inquirerQuestions = convertPromptToInquirerQuestions(selectedPrompt);
+  const answers = await inquirer.prompt(inquirerQuestions);
+
+  // Parse the prompt message
+  const parsedPrompt = parsePrompt(selectedPrompt, answers);
+
+  if (parsedPrompt.messages.length === 0) {
+    console.log("No messages found");
+    return;
+  }
+
+  if (parsedPrompt.messages.length === 1) {
+    console.log(parsedPrompt.messages[0].content);
+    return;
+  }
+
+  // Multiple messages
+  console.log(JSON.stringify(parsedPrompt.messages, null, 2));
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
