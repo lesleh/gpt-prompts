3d3881a54163e3c894d2d649a37da83b17493240
Leslie Hoare <iam@lesleh.co.uk>
2023-04-03 09:32:04 +0100

    Add error handling for unused variables
    Implemented function 'findExtraVariables' to identify any variables
    provided by the user that are not used in the prompt. The function then
    throws an error if any unused variables are found, informing the user
    about the unused variables. This change improves error handling by
    notifying the user of potential mistakes in their provided input.

--- src/parser.ts
+++ src/parser.ts
@@ -22,6 +22,25 @@ function parsePromptVariable(
 }
 
 /**
+ * Finds any extra variables provided by the user that are not used in the prompt.
+ *
+ * @param promptVariables {Record<string, PromptVariable>} - the variables defined in the prompt
+ * @param providedVariables - the variables provided by the user
+ * @returns the extra variables
+ *
+ * @internal
+ */
+function findExtraVariables(
+  promptVariables: Record<string, PromptVariable>,
+  providedVariables: Record<string, string>
+) {
+  const promptKeys = Object.keys(promptVariables);
+  const providedKeys = Object.keys(providedVariables);
+
+  return providedKeys.filter((key) => !promptKeys.includes(key));
+}
+
+/**
  * Parses the variables in a prompt and returns the parsed variables.
  * @param prompt - the prompt to parse
  * @param providedVariables - the variables to use in the prompt
@@ -32,6 +51,19 @@ export function parsePromptVariables(
   prompt: Pick<Prompt, "variables">,
   providedVariables: Record<string, string>
 ) {
+  const extraVariables = findExtraVariables(
+    prompt.variables,
+    providedVariables
+  );
+
+  if (extraVariables.length > 0) {
+    throw new Error(
+      `The following variables were provided but not used: ${extraVariables.join(
+        ", "
+      )}`
+    );
+  }
+
   const output = {} as Record<string, string>;
   for (const variableName in prompt.variables) {
     output[variableName] = parsePromptVariable(
@@ -39,7 +71,6 @@ export function parsePromptVariables(
       providedVariables[variableName]
     );
   }
-
   return output;
 }
 
